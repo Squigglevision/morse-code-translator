7ccc1812fed658803d0efb36cf099c97a5600f77
Laura H <127502256+Squigglevision@users.noreply.github.com>
2023-09-18 21:38:31 +1000

    Revert "Pull feature/testing into develop"

--- assets/javascript/domutils.js
+++
@@ -1,6 +0,0 @@
-export const replaceText = (elementType, text, parent) => {
-	const newElement = document.createElement(elementType);
-	const textNode = document.createTextNode(text);
-	newElement.appendChild(textNode);
-	parent.replaceChildren(newElement);
-};
